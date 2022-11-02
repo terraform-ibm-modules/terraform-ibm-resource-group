5a2c209519065cdbac8fda30dd5ee6710f4b3ca9
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2022-11-02 16:10:01 +0000

    fix: only return RG name output after group has been created

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,8 @@
 output "resource_group_name" {
   description = "Resource group name"
   value       = var.existing_resource_group_name != null ? data.ibm_resource_group.existing_resource_group[0].name : ibm_resource_group.resource_group[0].name
+  # explicit dependency required here to ensure resource group name is not returned until group is created
+  depends_on = [ibm_resource_group.resource_group]
 }
 
 output "resource_group_id" {
