e6e4fd452793529b8f820b99671143aa47aec0d7
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-11-16 11:32:15 +0000

    fix: remove depends_on from resource group name output (#51)

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,6 @@
 output "resource_group_name" {
   description = "Resource group name"
   value       = var.existing_resource_group_name != null ? data.ibm_resource_group.existing_resource_group[0].name : ibm_resource_group.resource_group[0].name
-  # explicit dependency required as workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4170
-  depends_on = [ibm_resource_group.resource_group]
 }
 
 output "resource_group_id" {
