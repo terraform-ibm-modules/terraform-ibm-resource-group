d2dbcd81d5107be131fff3d9970a5c7cd36ffe2c
Arya Girish K <aryagirish.kgit@gmail.com>
2025-04-04 16:24:43 +0530

    feat: improved user experience for validating input variable values (#706)

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@
 # Looks up resource group and creates a new one if none already exists
 ##############################################################################
 
-locals {
-  # Check only resource group was given or existing group was given but not both
-  # (approach based on https://stackoverflow.com/a/66682419)
-
-  ### Check if both groups were given
-  exist_both_resource_group_cdn = !(var.resource_group_name != null && var.existing_resource_group_name != null)
-  exist_both_resource_group_msg = "only 'resource_group_name' or 'existing_resource_group_name' can be given, but both have been given"
-  # tflint-ignore: terraform_unused_declarations
-  exist_both_resource_group_check = regex("^${local.exist_both_resource_group_msg}$", (local.exist_both_resource_group_cdn ? local.exist_both_resource_group_msg : ""))
-}
-
 data "ibm_resource_group" "existing_resource_group" {
   count = var.existing_resource_group_name != null ? 1 : 0
   name  = var.existing_resource_group_name
