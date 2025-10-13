fdddc9353397d79aef01588766c2f7a1b08e5c0d
Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>
2025-10-13 19:18:01 +0530

    Update outputs.tf

--- examples/new-resource-group/outputs.tf
+++ examples/new-resource-group/outputs.tf
@@ -7,3 +7,13 @@ output "resource_group_id" {
   value       = module.resource_group.resource_group_id
   description = "Resource group ID"
 }
+
+variable "IC_ENV_TAGS" {
+  default     = ""
+  type        = string
+  description = "leave blank if running locally. This variable will be automatically populated if running from an IBM Cloud Schematics workspace."
+}
+
+output "tags" {
+value = var.IC_ENV_TAGS
+}
